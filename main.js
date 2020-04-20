6c3d85862648579ac5cde6eb0fbee340a8493127
Dor Cohen <dordchn@gmail.com>
2020-04-20 21:30:13 +0300

    Moved tabata logic to new file

--- main.js
+++ main.js
@@ -1,23 +1,15 @@
-let tabataEvents;
-let tabataTime = 0;
-let tabataInterval = null;
 
-let player;
-let playerReady = false;
-
-// Load audios
-const audio_go = new Audio('sounds/321_go.mp3');
-const audio_rest = new Audio('sounds/321_rest.mp3');
-const audio_rounds = [];
-for (let i = 1; i <= 8; i++) {
-  audio_rounds.push(new Audio(`sounds/round_${i}.mp3`));
-}
+let youtubePlayer;
+let youtubePlayerReady = false;
+const tabataPlayer = new TabataPlayer();
+tabataPlayer.onaudiostart = () => youtubePlayer.setVolume(40);
+tabataPlayer.onaudioend = () => youtubePlayer.setVolume(100);
 
 function onYouTubeIframeAPIReady() {
-  player = new YT.Player('player', {
+  youtubePlayer = new YT.Player('player', {
     height: '390',
     width: '640',
-    videoId: '', // OPf0YbXqDm0
+    videoId: 'OPf0YbXqDm0', // OPf0YbXqDm0
     events: {
       'onReady': onPlayerReady,
       'onStateChange': onPlayerStateChange
@@ -26,68 +18,21 @@ function onYouTubeIframeAPIReady() {
 }
 
 function onPlayerReady(event) {
-  playerReady = true;
+  youtubePlayerReady = true;
 }
 
 document.querySelector('#start_btn').addEventListener('click', evt => {
-  if (playerReady) {
-    prepareTabata();
-    player.playVideo();
+  if (youtubePlayerReady) {
+    youtubePlayer.playVideo();
   }
 });
 
-document.querySelector('#pause_btn').addEventListener('click', () => player.pauseVideo());
+document.querySelector('#pause_btn').addEventListener('click', () => youtubePlayer.pauseVideo());
 
 function onPlayerStateChange(event) {
   if (event.data == YT.PlayerState.PLAYING) {
-    playTabata();
+    tabataPlayer.play();
   } else if (event.data == YT.PlayerState.PAUSED) {
-    pauseTabata();
+    tabataPlayer.pause();
   }
-}
-
-/************* Tabata *************/
-
-
-
-function prepareTabata() {
-  tabataEvents = {};
-  const addTabataAudio = (time, audio) => {
-    tabataEvents[Math.round(time * 10)] = audio;
-  };
-  for (let round = 0; round < 8; round++) {
-    startTime = round * 30;
-    addTabataAudio(startTime + 10 - audio_go.duration, audio_go);
-    addTabataAudio(startTime + 12, audio_rounds[round]);
-    addTabataAudio(startTime + 30 - audio_rest.duration, audio_rest);
-  }
-  console.log(tabataEvents)
-}
-
-function playTabata() {
-  if (tabataInterval) {
-    return;
-  }
-  tabataInterval = setInterval(evt => {
-    tabataTime += 1;
-    if (tabataEvents[tabataTime]) {
-      player.setVolume(40);
-      tabataEvents[tabataTime].currentTime = 0;
-      tabataEvents[tabataTime].play();
-
-      tabataEvents[tabataTime].onended = () => {
-        player.setVolume(100);
-      }
-    }
-  }, 100);
-}
-
-function pauseTabata() {
-  clearInterval(tabataInterval);
-  tabataInterval = null;
-}
-
-function stopTabata() {
-  pauseTabata();
-  tabataTime = 0;
 }
---
+++ tabata-player.js
@@ -0,0 +1,69 @@
+
+class TabataPlayer {
+  constructor() {
+    this.tabataEvents = null;
+    this.tabataTime = 0;
+    this.tabataInterval = null;
+    this.T = 100;
+    this.audioStartCallback = null;
+    this.audioEndCallback = null;
+
+    // Load audios
+    this.audio_go = new Audio('sounds/321_go.mp3');
+    this.audio_rest = new Audio('sounds/321_rest.mp3');
+    this.audio_rounds = [];
+    for (let i = 1; i <= 8; i++) {
+      this.audio_rounds.push(new Audio(`sounds/round_${i}.mp3`));
+    }
+  }
+
+  prepareEvents() {
+    this.tabataEvents = {};
+    const addTabataAudio = (time, audio) => this.tabataEvents[Math.round(time * 1000 / this.T)] = audio;
+    for (let round = 0; round < 8; round++) {
+      const startTime = round * 30;
+      addTabataAudio(startTime + 10 - this.audio_go.duration, this.audio_go);
+      addTabataAudio(startTime + 12, this.audio_rounds[round]);
+      addTabataAudio(startTime + 30 - this.audio_rest.duration, this.audio_rest);
+    }
+    console.log(this.tabataEvents);
+  }
+
+  play() {
+    if (this.tabataInterval) {
+      return;
+    }
+    if (!this.tabataEvents) {
+      this.prepareEvents();
+    }
+    this.tabataInterval = setInterval(evt => {
+      this.tabataTime += 1;
+      if (this.tabataEvents[this.tabataTime]) {
+        if (this.audioStartCallback) this.audioStartCallback();
+        this.tabataEvents[this.tabataTime].currentTime = 0;
+        this.tabataEvents[this.tabataTime].play();
+
+        this.tabataEvents[this.tabataTime].onended = () => {
+          if (this.audioEndCallback) this.audioEndCallback();
+        }
+      }
+    }, this.T);
+  }
+
+  pause() {
+    clearInterval(this.tabataInterval);
+    this.tabataInterval = null;
+  }
+
+  stop() {
+    pauseTabata();
+    tabataTime = 0;
+  }
+
+  set onaudiostart(callback) {
+    this.audioStartCallback = callback;
+  }
+  set onaudioend(callback) {
+    this.audioEndCallback = callback;
+  }
+}
